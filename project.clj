909ff26c5f6922a5a55b9018d626e9669eed1cbf
vemv <vemv@users.noreply.github.com>
2021-03-26 19:48:06 +0100

    Use test-all in CI
    Related: stop using :eval-in-leiningen, which can get on the way of using a specific Clojure version (and has other pitfalls)

--- project.clj
+++ project.clj
@@ -7,12 +7,7 @@
                 ;;*unchecked-math* :warn-on-boxed
                 }
   :source-paths ["src" "copied-deps"]
-  :dependencies [
-                 ;[org.clojure/clojure "1.5.1"]
-                 ;[org.clojure/clojure "1.6.0"]
-                 ;[org.clojure/clojure "1.7.0"]
-                 [org.clojure/clojure "1.8.0"]
-                 ;[org.clojure/clojure "1.9.0-master-SNAPSHOT"]
+  :dependencies [[org.clojure/clojure "1.10.2" :scope "provided"]
                  [org.clojars.brenton/google-diff-match-patch "0.1"]
                  [org.ow2.asm/asm-all "5.2"]]
   :profiles {:dev {:dependencies [[org.clojure/tools.macro "0.1.5"]
@@ -33,14 +28,12 @@
                                    [commons-io "2.4"]
                                    ]
                     :resource-paths ["test-resources"]}
-             :1.5 {:dependencies [[org.clojure/clojure "1.5.1"]]}
-             :1.6 {:dependencies [[org.clojure/clojure "1.6.0"]]}
              :1.7 {:dependencies [[org.clojure/clojure "1.7.0"]]}
              :1.8 {:dependencies [[org.clojure/clojure "1.8.0"]]}
              :1.9 {:dependencies [[org.clojure/clojure "1.9.0"]]}
              :1.10 {:dependencies [[org.clojure/clojure "1.10.2"]]}}
   :aliases {"test-all" ["with-profile"
-                        "dev,test,1.6:dev,test,1.7:dev,test,1.8:dev,test,1.9"
+                        "dev,test,1.7:dev,test,1.8:dev,test,1.9:dev,test,1.10"
                         "test"]}
   :eastwood {:source-paths ["src"]
              :test-paths ["test"]
@@ -48,17 +41,10 @@
              :debug #{}}
   :plugins [[net.assum/lein-ver "1.2.0"]]
   :lein-ver {:version-file "resources/EASTWOOD_VERSION"}
-  ;; Note: comment out the following line if you want to do 'lein
-  ;; test' and get a Clojure version later than 1.5.1, even if you
-  ;; have an explicit org.clojure/clojure in the :dependencies
-  ;; Also: Having the following line uncommented causes a problem when
-  ;; doing 'lein test', I think because of a different version of
-  ;; core.cache or core.memoize on which the latest tools.analyzer.jvm
-  ;; 0.1.0-SNAPSHOT depends, versus what Leiningen itself probably
-  ;; depends on.
-;;  :eval-in-leiningen true
   ;; Eastwood may work with earlier Leiningen versions, but this is
   ;; close to the earliest version that it was most tested with.
   :min-lein-version "2.3.0"
   :resource-paths ["resource" "resources"]
-  :eval-in-leiningen true)
+  :eval-in ~(case (System/getenv "EVAL_IN_LEININGEN")
+              ("1" "true") :leiningen
+              :subprocess))
